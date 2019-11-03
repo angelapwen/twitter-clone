0cdb0ffa9370fb434225a5a25f710c4639cd8f9b
Angela Wen <wena@seas.upenn.edu>
2019-11-02 18:31:58 -0700

    Add pull to refresh home timeline functionality

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -13,13 +13,18 @@ class HomeTableViewController: UITableViewController {
     var tweetArray = [NSDictionary]()
     var numOfTweets: Int!
     
+    let myRefreshControl = UIRefreshControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loadTweets()
+        
+        myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
     }
     
-    func loadTweets() {
+    @objc func loadTweets() {
         let homeTimelineUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         let myParams = ["count": 10]
         
@@ -32,6 +37,10 @@ class HomeTableViewController: UITableViewController {
             }
             
             self.tableView.reloadData()
+            
+            // End the refreshing
+            self.myRefreshControl.endRefreshing()
+            
         }, failure: { (Error) in
             print("Could not retrieve tweets")
         })
